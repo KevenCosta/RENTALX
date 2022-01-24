36f726af7eea7bd8b8b23a80099d37cd7fdf97ca
KevenCosta <kevencosta129@gmail.com>
2022-01-24 00:00:05 -0300

    M6 Config Rate Limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import "dotenv/config";
+import cors from "cors"
 import express, {Request, Response, NextFunction} from 'express'
 import "express-async-errors"//precisa ser depois do express
 import createConnection from "../../../shared/infra/typeorm"
@@ -9,13 +10,18 @@ import swaggerUi from "swagger-ui-express"
 import swaggerFile from "../../../swagger.json"
 import { AppError } from "../../errors/AppError"
 import upload from "../../../config/upload";
+import rateLimiter from "./middlewares/rateLimiter";
+
 
 createConnection();
 const app = express();
+app.use(rateLimiter);
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+
+app.use(cors())
 app.use(router);
 
 app.use((err:Error, request: Request, response: Response, next: NextFunction)=>{
